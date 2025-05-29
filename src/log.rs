2a59ad8f98d75bac78acf858aecf48b8d4bac5a7
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2025-05-29 11:50:47 +0200

    feat(log): initialize logging configuration from environment variables
    Updated the `init` function in the logging module to read
    configuration options for JSON and performance logging from
    environment variables. This change enhances flexibility and
    allows for dynamic logging configurations based on the
    environment.

--- src/log.rs
+++ src/log.rs
@@ -1,6 +1,12 @@
 use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
 
-pub fn init(log_json: bool, log_perf: bool) {
+use crate::env;
+
+pub fn init() {
+    // we avoid reading the lazy initialized ENV_CONFIG here as it depends on log being initialized
+    let log_json = env::get_env_bool("LOG_JSON").unwrap_or_default();
+    let log_perf = env::get_env_bool("LOG_PERF").unwrap_or_default();
+
     let builder = tracing_subscriber::fmt().with_env_filter(EnvFilter::from_default_env());
 
     let builder = if log_perf {
